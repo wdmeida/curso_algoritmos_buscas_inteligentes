f0945cb154cfaadab404403fb64ca73a1779f279
Wagner Almeida <wagner.almeida@inovamind.com.br>
2020-10-07 19:58:27 -0300

    Create project

---
+++ cidade.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+class Cidade:
+    def __init__(self, nome):
+        self.nome = nome
+        self.visitado = False
+        self.adjacentes = []
+        
+    def addCidadeAdjacente(self, cidade):
+        self.adjacentes.append(cidade)
+
